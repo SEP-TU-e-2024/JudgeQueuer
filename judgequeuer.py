0b170f1e1a26485e345acd829555ec545a24acfa
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2024-06-07 17:15:22 +0200

    Updated judgequeuer

--- judgequeuer.py
+++ judgequeuer.py
@@ -1,12 +1,15 @@
 import asyncio
 import os
+import socket
+import threading
 
 from dotenv import load_dotenv
 
-# from azureevaluator import AzureEvaluator
+from azureevaluator import AzureEvaluator
 from azurewrap import Azure
-
-# from models import JudgeRequest, MachineType, ResourceSpecification, Submission
+from models import JudgeRequest, MachineType, ResourceSpecification, Submission
+from protocol import Connection
+from protocol.judge import JudgeProtocol
 
 # Initialize environment variables from the `.env` file
 load_dotenv()
@@ -20,38 +23,50 @@ azure = Azure(SUBSCRIPTION_ID, RESOURCE_GROUP_NAME)
 HOST = "localhost"
 PORT = 12345
 
+
+def estabish_connection():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((HOST, PORT))
+    sock.listen(1000)
+
+    # while True:
+    #     client_sock, address = sock.accept()
+    #     connection = Connection(address[0], address[1], client_sock, threading.Lock())
+
+
 async def main():
-    pass
-    
-	# ae = AzureEvaluator(azure)
+    threading.Thread(target=estabish_connection, daemon=True).start()
 
-	# # Assign temporary values
-	
-	# # Assign submission information
-	# submission = Submission(1, "source_url")
+    ae = AzureEvaluator(azure)
 
-	# # Assign Machine type
-	# machine_type = MachineType("Standard_B1s", "Standard")
-	# # machine_type = MachineType("Standard_D2s_v3", "Standard")
-	# # machine_type = MachineType("Standard_D4s_v3", "Standard")
+    # Assign temporary values
 
-	# # Assign resource specification
-	# resource_allocation = ResourceSpecification(4, 32, 1, machine_type)
+    # Assign submission information
+    submission = Submission(1, "source_url")
 
-	# # Assign Judge request
-	# judge_request = JudgeRequest(submission, resource_allocation)
+    # Assign Machine type
+    machine_type = MachineType("Standard_B1s", "Standard")
+    # machine_type = MachineType("Standard_D2s_v3", "Standard")
+    # machine_type = MachineType("Standard_D4s_v3", "Standard")
 
-	# # Test out submitting judge request
-	# print("Submitting judge request...")
-	# print(await ae.submit(judge_request))
+    # Assign resource specification
+    resource_allocation = ResourceSpecification(4, 32, 1, machine_type)
+
+    # Assign Judge request
+    judge_request = JudgeRequest(submission, resource_allocation)
+
+    # Test out submitting judge request
+    print("Submitting judge request...")
+    print(await ae.submit(judge_request))
+
 
 if __name__ == "__main__":
-	# Wrap main to make sure all Azure objects are closed properly
-	async def wrap_main():
-		try:
-			await main()
-		finally:
-			await azure.close()
-	
-	# Run the main wrapper async
-	asyncio.run(wrap_main())
+    # Wrap main to make sure all Azure objects are closed properly
+    async def wrap_main():
+        try:
+            await main()
+        finally:
+            await azure.close()
+
+    # Run the main wrapper async
+    asyncio.run(wrap_main())
