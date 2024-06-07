e74b0bbf293738ed6a68bda0b6d6014b340fefa3
Andrei Tudor Popescu <a.popescu1@student.tue.nl>
2024-06-07 16:29:28 +0200

    Added protocol files

--- judgequeuer.py
+++ judgequeuer.py
@@ -3,9 +3,10 @@ import os
 
 from dotenv import load_dotenv
 
-from azureevaluator import AzureEvaluator
+# from azureevaluator import AzureEvaluator
 from azurewrap import Azure
-from models import JudgeRequest, MachineType, ResourceSpecification, Submission
+
+# from models import JudgeRequest, MachineType, ResourceSpecification, Submission
 
 # Initialize environment variables from the `.env` file
 load_dotenv()
@@ -16,28 +17,33 @@ RESOURCE_GROUP_NAME = os.getenv("AZURE_RESOURCE_GROUP_NAME")
 
 azure = Azure(SUBSCRIPTION_ID, RESOURCE_GROUP_NAME)
 
+HOST = "localhost"
+PORT = 12345
+
 async def main():
-	ae = AzureEvaluator(azure)
+    pass
+    
+	# ae = AzureEvaluator(azure)
 
-	# Assign temporary values
+	# # Assign temporary values
 	
-	# Assign submission information
-	submission = Submission(1, "source_url")
+	# # Assign submission information
+	# submission = Submission(1, "source_url")
 
-	# Assign Machine type
-	machine_type = MachineType("Standard_B1s", "Standard")
-	# machine_type = MachineType("Standard_D2s_v3", "Standard")
-	# machine_type = MachineType("Standard_D4s_v3", "Standard")
+	# # Assign Machine type
+	# machine_type = MachineType("Standard_B1s", "Standard")
+	# # machine_type = MachineType("Standard_D2s_v3", "Standard")
+	# # machine_type = MachineType("Standard_D4s_v3", "Standard")
 
-	# Assign resource specification
-	resource_allocation = ResourceSpecification(4, 32, 1, machine_type)
+	# # Assign resource specification
+	# resource_allocation = ResourceSpecification(4, 32, 1, machine_type)
 
-	# Assign Judge request
-	judge_request = JudgeRequest(submission, resource_allocation)
+	# # Assign Judge request
+	# judge_request = JudgeRequest(submission, resource_allocation)
 
-	# Test out submitting judge request
-	print("Submitting judge request...")
-	print(await ae.submit(judge_request))
+	# # Test out submitting judge request
+	# print("Submitting judge request...")
+	# print(await ae.submit(judge_request))
 
 if __name__ == "__main__":
 	# Wrap main to make sure all Azure objects are closed properly
