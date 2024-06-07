c8291f9ecb55eca5a5a2c779fa3f3fc78c190aca
Andrei Tudor Popescu <andreitudorp20@gmail.com>
2024-06-07 17:19:58 +0200

    Changed tabs to spaces (#11)

--- judgequeuer.py
+++ judgequeuer.py
@@ -17,35 +17,35 @@ RESOURCE_GROUP_NAME = os.getenv("AZURE_RESOURCE_GROUP_NAME")
 azure = Azure(SUBSCRIPTION_ID, RESOURCE_GROUP_NAME)
 
 async def main():
-	ae = AzureEvaluator(azure)
+    ae = AzureEvaluator(azure)
 
-	# Assign temporary values
-	
-	# Assign submission information
-	submission = Submission(1, "source_url")
+    # Assign temporary values
+    
+    # Assign submission information
+    submission = Submission(1, "source_url")
 
-	# Assign Machine type
-	machine_type = MachineType("Standard_B1s", "Standard")
-	# machine_type = MachineType("Standard_D2s_v3", "Standard")
-	# machine_type = MachineType("Standard_D4s_v3", "Standard")
+    # Assign Machine type
+    machine_type = MachineType("Standard_B1s", "Standard")
+    # machine_type = MachineType("Standard_D2s_v3", "Standard")
+    # machine_type = MachineType("Standard_D4s_v3", "Standard")
 
-	# Assign resource specification
-	resource_allocation = ResourceSpecification(4, 32, 1, machine_type)
+    # Assign resource specification
+    resource_allocation = ResourceSpecification(4, 32, 1, machine_type)
 
-	# Assign Judge request
-	judge_request = JudgeRequest(submission, resource_allocation)
+    # Assign Judge request
+    judge_request = JudgeRequest(submission, resource_allocation)
 
-	# Test out submitting judge request
-	print("Submitting judge request...")
-	print(await ae.submit(judge_request))
+    # Test out submitting judge request
+    print("Submitting judge request...")
+    print(await ae.submit(judge_request))
 
 if __name__ == "__main__":
-	# Wrap main to make sure all Azure objects are closed properly
-	async def wrap_main():
-		try:
-			await main()
-		finally:
-			await azure.close()
-	
-	# Run the main wrapper async
-	asyncio.run(wrap_main())
+    # Wrap main to make sure all Azure objects are closed properly
+    async def wrap_main():
+        try:
+            await main()
+        finally:
+            await azure.close()
+    
+    # Run the main wrapper async
+    asyncio.run(wrap_main())
--- models.py
+++ models.py
@@ -2,69 +2,69 @@ from enum import Enum
 
 
 class MachineType:
-	"""
-	A type of machine.
-	"""
-	# TODO: improve description
-	name: str
-	tier: str
+    """
+    A type of machine.
+    """
+    # TODO: improve description
+    name: str
+    tier: str
 
-	def __init__(self, name: str, tier: str):
-		self.name = name
-		self.tier = tier
+    def __init__(self, name: str, tier: str):
+        self.name = name
+        self.tier = tier
 
 class ResourceSpecification:
-	"""
-	The specification of allocated resources to evaluate a submission.
-	"""
-	num_cpu: int
-	num_memory: int
-	num_gpu: int
-	machine_type: 'MachineType'
+    """
+    The specification of allocated resources to evaluate a submission.
+    """
+    num_cpu: int
+    num_memory: int
+    num_gpu: int
+    machine_type: 'MachineType'
 
-	def __init__(self, num_cpu: int, num_memory: int, num_gpu: int, machine_type: 'MachineType'):
-		self.num_cpu = num_cpu
-		self.num_memory = num_memory # in MB
-		self.num_gpu = num_gpu
-		self.machine_type = machine_type
+    def __init__(self, num_cpu: int, num_memory: int, num_gpu: int, machine_type: 'MachineType'):
+        self.num_cpu = num_cpu
+        self.num_memory = num_memory # in MB
+        self.num_gpu = num_gpu
+        self.machine_type = machine_type
 
 class SubmissionType(Enum):
-	"""
-	The type of a submission: either code, or solution.
-	"""
-	CODE = 1
-	SOLUTION = 2
+    """
+    The type of a submission: either code, or solution.
+    """
+    CODE = 1
+    SOLUTION = 2
 
 
 class Submission:
-	"""
-	A submission that should be evaluated.
-	"""
-	type: 'SubmissionType'
-	source_url: str
+    """
+    A submission that should be evaluated.
+    """
+    type: 'SubmissionType'
+    source_url: str
 
-	def __init__(self, type: 'SubmissionType', source_url: str):
-		self.type = type
-		self.source_url = source_url
+    def __init__(self, type: 'SubmissionType', source_url: str):
+        self.type = type
+        self.source_url = source_url
 
 class JudgeRequest:
-	"""
-	A request for a submission to be evaluated according to some resource specification.
-	"""
-	submission: 'Submission'
-	resource_allocation: 'ResourceSpecification'
+    """
+    A request for a submission to be evaluated according to some resource specification.
+    """
+    submission: 'Submission'
+    resource_allocation: 'ResourceSpecification'
 
-	def __init__(self, submission: 'Submission', resource_allocation: 'ResourceSpecification'):
-		self.submission = submission
-		self.resource_allocation = resource_allocation
+    def __init__(self, submission: 'Submission', resource_allocation: 'ResourceSpecification'):
+        self.submission = submission
+        self.resource_allocation = resource_allocation
 
 class JudgeResult:
-	"""
-	The result of evaluation by a judge.
+    """
+    The result of evaluation by a judge.
 
-	Formatted in JSON.
-	"""
-	result: str
-	
-	def __init__(self, result: str):
-		self.result = result
+    Formatted in JSON.
+    """
+    result: str
+    
+    def __init__(self, result: str):
+        self.result = result
