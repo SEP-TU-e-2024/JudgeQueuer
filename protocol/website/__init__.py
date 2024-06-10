a2d5ba64c75519911e259f824cf244b656d872ec
TPGamesNL <pet.teun.03@gmail.com>
2024-06-10 11:55:57 +0200

    Copy files over from current version of protocol branch on website repo

---
+++ protocol/website/__init__.py
@@ -0,0 +1,3 @@
+"""
+This module contains the parts of the protocol used by the website.
+"""
---
+++ protocol/website/commands/__init__.py
@@ -0,0 +1,7 @@
+"""
+This module contains the commands for the runner.
+"""
+
+from .judge_commands import Commands
+
+__all__ = ["Commands"]
