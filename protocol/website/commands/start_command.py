379c90222bb9645d65c004cd80cc5c20d3863466
TPGamesNL <pet.teun.03@gmail.com>
2024-06-11 17:56:59 +0200

    Add start command implementation

--- protocol/website/commands/start_command.py
+++ protocol/website/commands/start_command.py
@@ -1,8 +1,17 @@
-"""
-This module contains the StartCommand class.
-"""
+import azureevaluator
+from custom_logger import main_logger
+from models import (
+    JudgeRequest,
+    MachineType,
+    ResourceSpecification,
+    Submission,
+    SubmissionType,
+)
 
 from .command import Command
+
+# Initialize the logger
+logger = main_logger.getChild("start_command")
 
 
 class StartCommand(Command):
@@ -11,5 +20,23 @@ class StartCommand(Command):
     """
 
     @staticmethod
-    def execute(args: dict):
-        return {"status": "ok"}
+    async def execute(args: dict):
+        # Deserialization of the arguments
+        machine_type = MachineType.from_name(args["machine_type"])
+        submission_type = {"code": SubmissionType.CODE, "solution": SubmissionType.SOLUTION}[args["submission_type"]]
+        resource_specification = ResourceSpecification(num_cpu=args["cpus"],
+                                                       num_memory=args["memory"],
+                                                       num_gpu=args["gpus"],
+                                                       machine_type=machine_type)
+        submission = Submission(submission_type, args["source_url"])
+        judge_request = JudgeRequest(submission, resource_specification)
+
+        # Submit the request to the evaluator
+        try:
+            judge_result = await azureevaluator.get_instance().submit(judge_request)
+
+            return {"status": "ok", "result": judge_result.result}
+        except Exception:
+            logger.error("An unexpected error has occured while trying to submit a judge request to the evaluator", exc_info=1)
+
+            return {"status": "error"}
