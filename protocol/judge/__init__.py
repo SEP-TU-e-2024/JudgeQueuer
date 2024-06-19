b288ce5f997446dac5a36cc46fa5ec617211450c
TPGamesNL <pet.teun.03@gmail.com>
2024-06-19 12:26:02 +0200

    Remove judge commands enum

--- protocol/judge/__init__.py
+++ protocol/judge/__init__.py
@@ -2,7 +2,6 @@
 This module contains the parts of the protocol used by the judge.
 """
 
-from .commands import Commands
 from .judge_protocol import JudgeProtocol
 
-__all__ = ["JudgeProtocol", "Commands"]
+__all__ = ["JudgeProtocol"]
